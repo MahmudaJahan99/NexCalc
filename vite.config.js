f1391cce00e48d9a115be2aca3ca6bdf32e47d0e
DepressedPotatok <mj.mjahan045@gmail.com>
2026-05-06 17:29:31 +0600

    ci: add GitHub Actions deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nexcalc/',
+  base: '/NexCalc/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
